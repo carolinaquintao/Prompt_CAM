778338877eac1343f79741402077c5f5d1c3fc0c
ANA C Q SIRAVENHA <carolinaquintao@gmail.com>
2026-05-30 16:18:21 -0300

    Update log_utils.py

--- utils/log_utils.py
+++ utils/log_utils.py
@@ -40,7 +40,7 @@ def collect_env_info():
     has_cuda = torch.cuda.is_available()
     data.append(("CUDA available", has_cuda))
     if has_cuda:
-        data.append(("CUDA ID", os.environ["CUDA_VISIBLE_DEVICES"]))
+        data.append(("CUDA ID", os.environ.get("CUDA_VISIBLE_DEVICES", "Not Set")))#data.append(("CUDA ID", os.environ["CUDA_VISIBLE_DEVICES"]))
         devices = defaultdict(list)
         for k in range(torch.cuda.device_count()):
             devices[torch.cuda.get_device_name(k)].append(str(k))
